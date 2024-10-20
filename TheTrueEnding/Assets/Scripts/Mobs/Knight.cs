9efcd5e625dc30f763810826cedce4ed8d7fafff
UkDopham <>
2024-10-20 21:21:31 +0200

    Adding Drag and Drop Item

--- TheTrueEnding/Assets/Scripts/Mobs/Knight.cs
+++ TheTrueEnding/Assets/Scripts/Mobs/Knight.cs
@@ -4,4 +4,29 @@ using UnityEngine;
 public class Knight : MonoBehaviour
 {
     private const float speed = 2f;
+    private List<ItemType> _items = new List<ItemType>();
+    private KnightMovement _knightMovement;
+
+    private void Awake()
+    {
+        this._knightMovement = GetComponent<KnightMovement>();
+    }
+    public void AddItem(ItemType type)
+    {
+        if (this._items.Contains(type))
+        {
+            return;
+        }
+
+        this._items.Add(type);
+    }
+
+    public void Hurt()
+    {
+        if(this._knightMovement == null)
+        {
+            return;
+        }
+        this._knightMovement.ChangeToOppositeDirection();
+    }
 }
