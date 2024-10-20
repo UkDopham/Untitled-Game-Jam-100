74d0d55a98cc3d59162956906e5696d73a2fc6da
UkDopham <>
2024-10-21 00:15:53 +0200

    Arrow direction

--- TheTrueEnding/Assets/Scripts/Items/Arrow.cs
+++ TheTrueEnding/Assets/Scripts/Items/Arrow.cs
@@ -4,18 +4,21 @@ using UnityEngine.Tilemaps;
 public class Arrow : MonoBehaviour
 {
     [SerializeField]
-    private float _speed = 8f;           
-    private Vector3 _direction;        
+    private float _speed = 8f;
+    private Vector3 _direction;
 
     public void SetTarget(Vector3 targetPosition)
     {
-        this._direction = (targetPosition - transform.position).normalized;
+        this._direction = (targetPosition - this.transform.position).normalized;
+        float angle = Mathf.Atan2(this._direction.y, this._direction.x) * Mathf.Rad2Deg;
+        this.transform.rotation = Quaternion.Euler(0, 0, angle - 90);
     }
 
     private void Update()
     {
-        transform.Translate(this._direction * this._speed * Time.deltaTime, Space.World);
+        this.transform.Translate(this._direction * this._speed * Time.deltaTime, Space.World);
     }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.GetComponent<Knight>() != null)
@@ -25,11 +28,11 @@ public class Arrow : MonoBehaviour
             {
                 knight.Hurt();
             }
-            Destroy(gameObject);
+            Destroy(this.gameObject);
         }
         else if (other.GetComponent<TilemapCollider2D>() != null)
         {
-            Destroy(gameObject);
+            Destroy(this.gameObject);
         }
     }
 }
