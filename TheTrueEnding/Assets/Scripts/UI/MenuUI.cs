539602a0301f7590addbc3743b0651d46ad1af4e
UkDopham <>
2024-10-27 10:58:51 +0100

    UI Sounds

--- TheTrueEnding/Assets/Scripts/UI/MenuUI.cs
+++ TheTrueEnding/Assets/Scripts/UI/MenuUI.cs
@@ -3,6 +3,8 @@ using UnityEngine.SceneManagement;
 
 public class MenuUI : MonoBehaviour
 {
+    [SerializeField]
+    private AudioSource _audioSource;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -16,18 +18,34 @@ public class MenuUI : MonoBehaviour
     }
     public void PlayOnClick()
     {
+        if(this._audioSource != null)
+        {
+            this._audioSource.Play();
+        }
         SceneManager.LoadScene((int)Scene.Level);
     }
     public void TutorialOnClick()
     {
+        if (this._audioSource != null)
+        {
+            this._audioSource.Play();
+        }
         SceneManager.LoadScene((int)Scene.Tutorial);
     }
     public void CreditsOnClick()
     {
+        if (this._audioSource != null)
+        {
+            this._audioSource.Play();
+        }
         SceneManager.LoadScene((int)Scene.Credits);
     }
     public void MenuOnClick()
     {
+        if (this._audioSource != null)
+        {
+            this._audioSource.Play();
+        }
         SceneManager.LoadScene((int)Scene.Menu);
     }
 }
